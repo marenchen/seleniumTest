cb516f5ed2e16a9dd035393a4beb01c90a978e61
marenchen <70327913+marenchen@users.noreply.github.com>
2020-09-02 09:02:04 +0200

    Add files via upload

---
+++ src/test/java/first_maven_project/TestAndagon.java
@@ -0,0 +1,46 @@
+package first_maven_project;
+
+import org.junit.After; 
+import org.junit.Before; 
+import org.junit.Test;
+import org.junit.Assert; 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class TestAndagon {
+	
+	WebDriver wDriver;
+	String sBaseUrl = "https://www.andagon.com/";
+	
+	@Before 
+	public void setUp() throws Exception{ 
+		System.setProperty("webdriver.chrome.driver", "C:/Selenium/chromedriver.exe"); 
+		wDriver = new ChromeDriver();
+		wDriver.manage().window().maximize();
+		wDriver.get(sBaseUrl);
+	}
+	
+	
+	@Test 
+	public void test() throws Exception{ 
+		WebDriverWait wait = new WebDriverWait(wDriver, 10);
+		WebElement andagonLogo = wDriver.findElement(By.xpath("//img[@class='header__logo']"));
+		andagonLogo.click();
+		wait.until(ExpectedConditions.elementToBeClickable(wDriver.findElement(By.xpath("//img[@class='header__logo']"))));
+		String currentUrl = wDriver.getCurrentUrl();
+		Assert.assertEquals(sBaseUrl, currentUrl);
+		
+	}
+
+	@After 
+	public void tearDown(){ 
+		wDriver.quit(); 
+	} 
+
+}
